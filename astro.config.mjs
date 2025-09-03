c6050a579a9a5057806dd019327049b8dadb3f7a
zkjon <121103151+zkj0n@users.noreply.github.com>
2025-09-03 10:37:45 +0200

    feat: add tailwind css dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+import path from 'path';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        '@': path.resolve('./src'),
+      },
+    },
+  },
+});
